612f04639b3a3921184898a4767c85a51afb692d
corpusmarc <marccorpus.work@gmail.com>
2024-03-01 19:21:45 +0800

    Update SafeArea

--- src/components/SafeArea.js
+++ src/components/SafeArea.js
@@ -1,9 +1,16 @@
+import { StyleSheet } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function SafeArea({ children }) {
   return (
     <SafeAreaProvider>
-      <SafeAreaView>{children}</SafeAreaView>
+      <SafeAreaView style={styles.container}>{children}</SafeAreaView>
     </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
