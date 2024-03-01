7538e7694b1cab425cf11d8f3278f7aa71573683
corpusmarc <marccorpus.work@gmail.com>
2024-03-01 19:15:16 +0800

    Create dummy data

---
+++ src/data/categories/index.js
@@ -0,0 +1,14 @@
+import Category from "./model";
+
+export default [
+  new Category("c1", "Italian"),
+  new Category("c2", "Quick & Easy"),
+  new Category("c3", "Hamburgers"),
+  new Category("c4", "German"),
+  new Category("c5", "Light & Lovely"),
+  new Category("c6", "Exotic"),
+  new Category("c7", "Breakfast"),
+  new Category("c8", "Asian"),
+  new Category("c9", "French"),
+  new Category("c10", "Summer"),
+];
