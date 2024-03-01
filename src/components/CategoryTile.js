b7ab3f18f6c56fd41f806ee456b0ce245fd4bf46
corpusmarc <marccorpus.work@gmail.com>
2024-03-01 22:19:14 +0800

    Update Category Tile component

--- src/components/CategoryTile.js
+++ src/components/CategoryTile.js
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: colors.lightgray,
-    borderRadius: 8,
+    borderRadius: 12,
   },
   text: {
     fontSize: 18,
